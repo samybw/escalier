8c390de46d0caad88c2806a0bcd0f6d3e5a7c01f
samy <samybn@yahoo.com>
2020-05-25 09:18:09 +0200

    l'escalier est a l'envers mais le divise par 2

--- escalier.js
+++ escalier.js
@@ -1,7 +1,8 @@
-var nombre = 8;
+var nombre = 4;
 var diese = "#";
+var space = " "
 for (let index = 1; index <= nombre ; index++) {
-    console.log(diese.repeat(index))
+    console.log(space.repeat(nombre--)+diese.repeat(index))
 
     
 }
