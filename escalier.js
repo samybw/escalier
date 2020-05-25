392c72d7b081011e594c9cb12537f075119067c6
samy <samybn@yahoo.com>
2020-05-25 09:40:36 +0200

    creation de l'esclaier a l'envers il manque plus qu'a trouve un moyen de prompt

--- escalier.js
+++ escalier.js
@@ -3,10 +3,7 @@ var i = 1;
 var j = nombre
 var diese = "#";
 var space = " "
-/*for (let index = 1; index <= nombre ; index++) {
-   
-        console.log(space.repeat(nombre -= 1)+diese.repeat(index))
-}*/
+
 
 while (i <= nombre || j >= i) {
     console.log(space.repeat(j--)+diese.repeat(i++));
