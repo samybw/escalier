8390e5c39e6011dc65aee0eafe1adcde34351b4a
samy <samybn@yahoo.com>
2020-05-25 09:38:41 +0200

    ajout des variable space et j pour crée un escalier inverse

--- escalier.js
+++ escalier.js
@@ -1,8 +1,14 @@
-var nombre = 4;
+var nombre = 6;
+var i = 1;
+var j = nombre
 var diese = "#";
 var space = " "
-for (let index = 1; index <= nombre ; index++) {
-    console.log(space.repeat(nombre--)+diese.repeat(index))
+/*for (let index = 1; index <= nombre ; index++) {
+   
+        console.log(space.repeat(nombre -= 1)+diese.repeat(index))
+}*/
 
+while (i <= nombre || j >= i) {
+    console.log(space.repeat(j--)+diese.repeat(i++));
     
 }
